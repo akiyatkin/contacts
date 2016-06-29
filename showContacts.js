3dd5daedde9302a985d94c46d8eb8c068fda90fb
Anton <a.kiyatkin@itlf.ru>
2016-06-29 14:27:44 +0400

    fix

--- showContacts.js
+++ showContacts.js
@@ -5,7 +5,13 @@ window.contacts={
 		config:{}
 	},
 	show:function(){
-		popup.open(this.popup);
+		var layer = this.popup;
+		popup.open(layer);
+		
+		Event.one('layer.onshow', function () {
+			infrajs.popup_memorize('contacts.show()');
+		},'', layer);
+		
 	},
 	popup:{},
 	layer:{
@@ -15,13 +21,31 @@ window.contacts={
 		divcheck:true
 	}
 }
+contacts.extlayer.onsubmit = function (layer) {
+	var config = layer.config;
+	var div = $('#'+layer.div);
+	if (!config.ans) {
+		config.ans = {
+			"result":0, 
+			"msg":"Произошла ошибка.<br>Cообщение не отправлено..."
+		}
+	}
+	if (config.ans.result) {
+		div.find('textarea').val('').change();
+	}
+	var conf = Config.get('contacts');
+	if (conf.yaCounter) {
+		var ya = window['yaCounter' + conf.yaCounter];
+		if (ya) {
+			console.info('reachGoal');
+			ya.reachGoal(conf.yaGoal, config.ans)
+		}
+	}
+}
 contacts.popup.external=contacts.extlayer;
 contacts.layer.external=contacts.extlayer;
 
-//infra.when(infrajs,'Infrajs.oninit',function(){//depricated
-//	infrajs.checkAdd(contacts.layer);//должна добавиться после того как основные слои добавятся, и при этом участвовать в первой пробежке
-//});
-Event.handler('Infrajs.onshow',function(){
+Event.handler('Controller.onshow',function(){
 	$('.showContacts[showContacts!=true]').attr('infra','false').attr('showContacts','true').click(function(){
 		if($(this).data('text')){
 			if(!infra.session.get('user.text')){
