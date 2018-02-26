668894ea45e5e13ca892cb68a60ffff545f19eb2
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-02-27 00:46:40 +0400

    add boo cache

--- js.php
+++ js.php
@@ -3,20 +3,13 @@ namespace infrajs\contacts;
 use infrajs\router\Router;
 use infrajs\ans\Ans;
 use infrajs\config\Config;
-use infrajs\cache\Cache;
-
-if (!is_file('vendor/autoload.php')) {
-	chdir('../../../'); //Согласно фактическому расположению файла
-	require_once('vendor/autoload.php');
-	Router::init();
-}
+use akiyatkin\boo\Cache;
 
 $conf = Config::get('contacts');
-if ($conf['reCAPTCHA']) {
-	$date = date('d.m.Y');
-	$js = Cache::exec(array(), __FILE__, function ($date) {
+if (!$conf['reCAPTCHA']) {
+	$js = Cache::exec('Скрипт Google reCAPTHA', function () {
 		return file_get_contents('https://www.google.com/recaptcha/api.js?onload=grecaptchaOnload&render=explicit&hl=ru');
-	}, array($date));
+	}, array(), ['akiyatkin\boo\Cache','getDurationTime'], array('last friday'));
 	$js .= 'window.grecaptchaOnload=function(){ Event.fire("reCAPTCHA") };';
 
 } else {
