6fb716e65f1f7afb08b26fad4fc86d62aa1d878e
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-07 17:56:45 +0400

    fix

--- showContacts.js
+++ showContacts.js
@@ -1,96 +1,89 @@
-window.contacts={
-	extlayer:{
-		divs:{},
-		tplroot:"start",
-		external:'-contacts/contacts.layer.json',
-		config:{}
+window.contacts = {
+	extlayer: {
+		divs: {},
+		tplroot: "start",
+		external: '-contacts/contacts.layer.json',
+		config: {}
 	},
-	show:async function(data){
-		let CDN = (await import('/vendor/akiyatkin/load/CDN.js')).default
+	show: async function (data) {
+		let { CDN } = await import('/vendor/akiyatkin/load/CDN.js')
 		await CDN.load('jquery')
 		var layer = this.popup
-		
+
 		if (!layer.config) layer.config = {};
 		layer.config.data = data;
 
-		popup.open(layer);
+		let { Popup } = await import('/vendor/infrajs/popup/Popup.js')
+		Popup.open(layer);
+
+		let { Wait } = await import('/vendor/akiyatkin/load/Wait.js')
+		await Wait()
 
 		Event.one('Layer.onshow', function () {
-			infrajs.popup_memorize("contacts.show("+JSON.stringify(data)+")");
-		},'', layer);
-		
+			Controller.popup_memorize("contacts.show(" + JSON.stringify(data) + ")");
+		}, '', layer);
+
 	},
-	popup:{},
-	layer:{
-		onlyclient:true,
-		autofocus:true, 
-		div:"showContacts",
-		divcheck:true
+	popup: {},
+	layer: {
+		onlyclient: true,
+		autofocus: true,
+		div: "showContacts",
+		divcheck: true
 	}
 }
 contacts.extlayer.onsubmit = function (layer) {
 	var config = layer.config;
-	var div = $('#'+layer.div);
+	var div = $('#' + layer.div);
 	if (!config.ans) {
 		config.ans = {
-			"result":0, 
-			"msg":"Произошла ошибка.<br>Cообщение не отправлено..."
+			"result": 0,
+			"msg": "Произошла ошибка.<br>Cообщение не отправлено..."
 		}
 	}
 	if (config.ans.result) {
 		div.find('textarea').val('').change();
 	}
-	/*
-		Перенесено в шаблон
-		var conf = Config.get('contacts');
-		if (conf.yaCounter) {
-			var ya = window['yaCounter' + conf.yaCounter];
-			if (ya) {
-				console.info('reachGoal');
-				ya.reachGoal(conf.yaGoal, config.ans)
-			}
-		}
-	*/
 }
-contacts.popup.external=contacts.extlayer;
-contacts.layer.external=contacts.extlayer;
+contacts.popup.external = contacts.extlayer;
+contacts.layer.external = contacts.extlayer;
 
 contacts.callback_layer = {
-	"external":"-contacts/callback/layer.json"
+	"external": "-contacts/callback/layer.json"
 }
 Event.handler('Controller.onshow', async () => {
 	let CDN = (await import('/vendor/akiyatkin/load/CDN.js')).default
 	await CDN.load('jquery');
-	$('.showContacts[showContacts!=true]').attr('infra','false').attr('showContacts','true').click( function() {
+	$('.showContacts[showContacts!=true]').attr('infra', 'false').attr('showContacts', 'true').click(function () {
 		var data = $(this).data();
 		if ($(this).data('text')) {
 			if (!Session.get('user.text')) {
-				Session.set('user.text', $(this).data('text'), false, function(){
+				Session.set('user.text', $(this).data('text'), false, function () {
 					contacts.show(data);
 				});
 			}
 		}
 		if ($(this).data('replace')) {
-			Session.set('user.text', $(this).data('replace'), false, function(){
+			Session.set('user.text', $(this).data('replace'), false, function () {
 				contacts.show(data);
 			});
 		} else {
-			contacts.show(data);	
+			contacts.show(data);
 		}
-		
 		return false;
 	});
 
 
-	
+
 	let cls = cls => document.getElementsByClassName(cls)
 	let list = cls('showCallback')
-	for (let i = 0, l = list.length; i < l; i++ ) {
+	for (let i = 0, l = list.length; i < l; i++) {
 		let el = list[i]
 		if (el.showCallback) continue
 		el.showCallback = true
-		el.addEventListener('click', () => {
-			Popup.show(contacts.callback_layer);
+		el.addEventListener('click', async () => {
+			let { Popup } = await import('/vendor/infrajs/popup/Popup.js')
+			Popup.show(contacts.callback_layer)
 		})
 	}
 
